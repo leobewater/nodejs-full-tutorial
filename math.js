1408447c592ca341218963caaab8ca0392ef4cfc
leobewater <mmbee888@gmail.com>
2023-09-29 16:11:23 -0400

    module.exports

---
+++ math.js
@@ -0,0 +1,11 @@
+module.exports.add = (a, b) => {
+  // const add = (a, b) => {
+  return a + b;
+};
+
+module.exports.subtract = (a, b) => {
+  //const subtract = (a, b) => {
+  return a - b;
+};
+
+// module.exports = { add, subtract };
