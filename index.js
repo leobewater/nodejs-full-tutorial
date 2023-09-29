0214cf7608d279d2426ee5cd5264eddd7ebd26fe
leobewater <mmbee888@gmail.com>
2023-09-29 16:05:56 -0400

    module caching

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-require('./batman');
-require('./superman');
+// when loading a new module, it caches for the subsequence loading
+/*
+const superHero = require('./super-hero');
+console.log(superHero.getName()); // Batman
+
+superHero.setName('Superman');
+console.log(superHero.getName()); // Superman
+
+// using the module caching
+const newSuperHero = require('./super-hero');
+console.log(newSuperHero.getName()); // Superman
+*/
+
+const SuperHero = require('./super-hero');
+
+const batman = new SuperHero('Batman');
+console.log(batman.getName());
+batman.setName('Bruce Wayne');
+console.log(batman.getName());
+
+const superman = new SuperHero('Superman');
+console.log(superman.getName());
