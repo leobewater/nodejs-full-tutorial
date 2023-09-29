23c4fff9179967f4b49fbf54e52c278b96a706d9
leobewater <mmbee888@gmail.com>
2023-09-29 15:49:03 -0400

    iife

---
+++ iife.js
@@ -0,0 +1,11 @@
+(function (message) {
+  const superHero = 'Batman';
+  console.log(message, superHero);
+})("Hello");
+
+(function (message) {
+  const superHero = 'Superman';
+  console.log(message, superHero);
+})("Hey");
+
+// under the hook, node.js using the iife scope
