37c29695db199749cf31850760e7e6388b739cfd
leobewater <mmbee888@gmail.com>
2023-09-30 10:59:38 -0400

    customize thread pool size

---
+++ 18-libuv-thread-pool-size/index.js
@@ -0,0 +1,20 @@
+const crypto = require('node:crypto');
+
+// libuv - has 4 threads by default
+// you can customize the pool size, but should be within the CPU core
+// if pool size over CPU core, it will slow down everything
+process.env.UV_THREADPOOL_SIZE = 5;
+
+// crypto using thread pool to do some of the tasks
+const start = Date.now();
+
+const MAX_CALLS = 16;
+
+for (let i = 0; i < MAX_CALLS; i++) {
+  // Async
+  crypto.pbkdf2('password', 'salt', 100000, 512, 'sha512', () => {
+    // total 369 as running in parallel
+    // hash 5 takes more time by default without the customized pool size  
+    console.log(`Hash: ${i + 1}`, Date.now() - start, 'ms'); 
+  });
+}
