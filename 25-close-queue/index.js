62e20a6fbb38b21ee3e239a7b1b9232f1b66da08
leobewater <mmbee888@gmail.com>
2023-09-30 13:35:44 -0400

    close queue

---
+++ 25-close-queue/index.js
@@ -0,0 +1,27 @@
+// Close Queue
+const fs = require('fs');
+
+const readableStream = fs.createReadStream(__filename);
+readableStream.close();
+
+// listener
+readableStream.on('close', () => {
+  console.log('this is from readableStream close event callback');
+})
+
+setImmediate(() => console.log('this is setImmediate 1'));
+setTimeout(() => console.log('this is setTimeout 1'), 0);
+Promise.resolve().then(() => console.log('this is Promise.resolve 1'));
+process.nextTick(() => console.log('this is process.nextTick 1'));
+
+console.log('done');
+
+/*
+Result:
+done
+this is process.nextTick 1
+this is Promise.resolve 1
+this is setTimeout 1
+this is setImmediate 1
+this is from readableStream close event callback
+*/
