ce6d676627f58eb2b57d986e95ae41396c175a63
leobewater <mmbee888@gmail.com>
2023-09-30 07:45:55 -0400

    events

---
+++ 07-events/index.js
@@ -0,0 +1,18 @@
+const EventEmitter = require('node:events');
+
+const emitter = new EventEmitter();
+
+// register listeners (non-blocking)
+emitter.on('order-pizza', (size, topping) => {
+  console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+});
+emitter.on('order-pizza', (size) => {
+  if (size === 'large') {
+    console.log(`Serving compleimentary drink`);
+  }
+});
+
+console.log("Do work before event occurs in the system"); // appear first
+
+// dispatch an event with payload
+emitter.emit('order-pizza', 'large', 'mushroom');
