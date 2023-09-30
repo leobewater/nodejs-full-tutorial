7bff996edc5b2ee85ab2e86c95d30daace55a53f
leobewater <mmbee888@gmail.com>
2023-09-30 10:04:01 -0400

    load external html file

--- 14-http-module/index.js
+++ 14-http-module/index.js
@@ -1,15 +1,23 @@
 const http = require('node:http');
+const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-  const superHero = {
-    firstName: 'Bruce',
-    lastName: 'Wayne',
-  };
+  //   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  //   res.end('Hello World');
 
-  //res.writeHead(200, { 'Content-Type': 'text/plain' });
-  //res.end('Hello World');
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(superHero));
+  //   const superHero = {
+  //     firstName: 'Bruce',
+  //     lastName: 'Wayne',
+  //   };
+  //   res.writeHead(200, { 'Content-Type': 'application/json' });
+  //   res.end(JSON.stringify(superHero));
+
+  //   res.writeHead(200, { 'Content-Type': 'text/html' });
+  //   res.end('<h1>Hello World</h1>');
+
+  const html = fs.readFileSync('./index.html', 'utf-8');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(html);
 });
 
 server.listen(3000, () => {
