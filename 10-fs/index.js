c65cb46bde2191ba53fab33efc3cd89d8c89759c
leobewater <mmbee888@gmail.com>
2023-09-30 08:33:20 -0400

    .writeFile vs .writeFileSync

--- 10-fs/index.js
+++ 10-fs/index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 
-console.log("First");
+console.log('First');
 
 // const fileContents = fs.readFileSync("./file.txt");
 // console.log(fileContents); // <Buffer 48 65 6c 6c 6f 20 43 6f 64 65 76 6f 6c 75 74 69 6f 6e>
@@ -12,7 +12,7 @@ console.log(fileContents); // Hello Codevolution
 console.log('Second');
 
 // async read file
-fs.readFile('./file.txt', "utf-8", (error, data) => {
+fs.readFile('./file.txt', 'utf-8', (error, data) => {
   if (error) {
     console.log(error);
   } else {
@@ -21,3 +21,14 @@ fs.readFile('./file.txt', "utf-8", (error, data) => {
 });
 
 console.log('Third'); // this appears before the .readFile()
+
+fs.writeFileSync('./greet.txt', 'Hello world!');
+
+// by default .writeFile override the file, unless adding a flag for append
+fs.writeFile('./greet.txt', ' Hello Vishwas!', { flag: 'a' }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('File written');
+  }
+});
