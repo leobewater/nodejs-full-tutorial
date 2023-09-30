068b44eb6c7bd1bb87b958607a8bfb35fc37b60f
leobewater <mmbee888@gmail.com>
2023-09-30 11:02:28 -0400

    use cpu core length

--- 18-libuv-thread-pool-size/index.js
+++ 18-libuv-thread-pool-size/index.js
@@ -1,9 +1,11 @@
 const crypto = require('node:crypto');
+const OS = require('os')
 
+console.log(OS.cpus().length);
 // libuv - has 4 threads by default
 // you can customize the pool size, but should be within the CPU core
 // if pool size over CPU core, it will slow down everything
-process.env.UV_THREADPOOL_SIZE = 5;
+process.env.UV_THREADPOOL_SIZE = OS.cpus().length; //8
 
 // crypto using thread pool to do some of the tasks
 const start = Date.now();
